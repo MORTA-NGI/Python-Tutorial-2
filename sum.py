132c3db8334190e722f8a9e8d576f1dd4ddc56c2
MORTA-NGI <mortdaa@ngi.ac.za>
2024-09-29 09:47:01 +0200

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,13 @@
+def sum(number1, number2):
+    sum =number2+number1
+    return sum   #8
+
+
+no1=int(input("Enter the number 1: "))#10
+no2=int(input("Enter the number 2: "))#20
+
+result=sum(no1, no2) # 30
+result2=result-20#10
+
+print('result ' ,result)#
+print('result2 ' , result2)#
